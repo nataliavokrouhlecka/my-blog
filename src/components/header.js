4a3d2cc26568d8c474f8ca35b7b9215f65c9618a
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2020-02-27 20:11:53 +0100

    Add class name

--- src/components/header.js
+++ src/components/header.js
@@ -7,9 +7,11 @@ class Header extends React.Component {
   render() {
     return (
       <div id="about" className="header">
-        <h2 className="header-tagline">Hi, I'm Natalia! I'm a web developer.</h2>
-        <p>I enjoy organising workshops for developers. I also teach children programming using Scratch and Ozobots.
+        <div className="header-content">
+          <h2 className="header-tagline">Hi, I'm Natalia! I'm a web developer.</h2>
+          <p className="header-text">I enjoy organising workshops for developers. I also teach children programming using Scratch and Ozobots.
 I am planning to share here my experience and ideas as well as experiment a bit with some new technologies - it is the excuse in case something goes wrong and the blog is down or broken ¯\_(ツ)_/¯.</p>
+        </div>
       </div>
     );
   }
