30a150e6c45cd712c0d1daa65d22ae90cae46308
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2020-02-27 20:09:24 +0100

    Change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,5 +24,17 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "xxx",
+        short_name: "xxxx",
+        start_url: "/",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png" // This path is relative to the root of the site.
+      }
+    }
   ],
 }
