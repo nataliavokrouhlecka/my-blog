1223b461fc63c32a90fa5ddbad19e7ecb5987255
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2020-02-27 20:12:24 +0100

    Update events style

--- src/components/events.js
+++ src/components/events.js
@@ -8,11 +8,11 @@ class Events extends React.Component {
     return (
       <div id="events" className="events">
         <h2 className="events-header">I enjoy organising events and enabling people to come together, learn and grow. It always helps me to learn something new myself. I co-led all of the events listed below.</h2>
-        <Timeline mode="alternate">
-          <Timeline.Item color="green">Reactive Conf, Code Retreat workshop 1-Nov-2019</Timeline.Item>
-          <Timeline.Item color="green">Open meetup at MSD, Hacktoberfest workshop 23-Oct-2019</Timeline.Item>
-          <Timeline.Item color="green">Open meetup at MSD, Code Retreat workshop 11-Jun-2019</Timeline.Item>
-          <Timeline.Item color="green">Dev Fest, Code Retreat workshop 2-Nov-2018</Timeline.Item>
+        <Timeline mode="alternate" className="events-timeline">
+          <Timeline.Item color="#FFF8D1">Reactive Conf, Code Retreat workshop 1-Nov-2019</Timeline.Item>
+          <Timeline.Item color="#FFF8D1">Open meetup at MSD, Hacktoberfest workshop 23-Oct-2019</Timeline.Item>
+          <Timeline.Item color="#FFF8D1">Open meetup at MSD, Code Retreat workshop 11-Jun-2019</Timeline.Item>
+          <Timeline.Item color="#FFF8D1">Dev Fest, Code Retreat workshop 2-Nov-2018</Timeline.Item>
         </Timeline>
       </div>
     );
