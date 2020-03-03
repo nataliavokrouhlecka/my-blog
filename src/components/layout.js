65fa03ce680141a230713038776ee1437761b00e
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2020-03-03 18:31:25 +0100

    Remove unused code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,38 +1,16 @@
 import React from "react"
-import { useStaticQuery, Link, graphql } from "gatsby"
 import TopMenu from "../components/menu"
 import Footer from "../components/footer"
-
+import './layout.css'
 
 export default ({ children }) => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `
-  )
   return (
-    <>
-      <TopMenu />
-      <Link to={`/`}>
-        {/* <h3
-          css={css`
-            margin-bottom: ${rhythm(2)};
-            margin: 0 auto;
-            display: inline-block;
-            font-style: normal;
-          `}
-        >
-          {data.site.siteMetadata.title}
-        </h3> */}
-      </Link>
-      {children}
+    <div className="page-container">
+      <div className="content-wrap">
+        <TopMenu />
+        {children}
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
