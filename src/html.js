d4f0fff55aa0f53c4bb4f57a25d05548ad592bc4
nataliavokrouhlecka <natalia.vokrouhlecka@gmail.com>
2020-03-08 21:21:01 +0100

    Add GA tracking

--- src/html.js
+++ src/html.js
@@ -5,13 +5,21 @@ export default function HTML(props) {
   return (
     <html lang="en" {...props.htmlAttributes}>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160002649-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'UA-160002649-1');
+        </script>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link href="https://fonts.googleapis.com/css?family=Architects+Daughter|Roboto:400,500,700&display=swap" rel="stylesheet" /> 
+        <link href="https://fonts.googleapis.com/css?family=Architects+Daughter|Roboto:400,500,700&display=swap" rel="stylesheet" />
         <script src="https://kit.fontawesome.com/190e01535f.js" crossOrigin="anonymous" samesite="none Secure"></script>
         {props.headComponents}
         <title>Natalia's blog</title>
